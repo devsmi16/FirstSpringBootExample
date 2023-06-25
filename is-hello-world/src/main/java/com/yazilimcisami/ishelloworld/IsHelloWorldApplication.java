3e1a9cebe8f667059ee5f413ad1a41061f0c3df1
Sami Gündoğan <102728972+devsmi16@users.noreply.github.com>
2023-06-25 15:08:46 +0300

    Add files via upload

---
+++ is-hello-world/src/main/java/com/yazilimcisami/ishelloworld/IsHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.yazilimcisami.ishelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IsHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsHelloWorldApplication.class, args);
+	}
+}
